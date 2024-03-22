6665e3104b7296955e1629f8ec89700b5a66a657
ahmadAs123 <83554259+ahmadAs123@users.noreply.github.com>
2024-03-22 16:28:11 +0200

    the search feild component for the client page

---
+++ component/SearchFeild.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { StyleSheet, Alert, View } from 'react-native';
+import IoniconsIcon from 'react-native-vector-icons/Ionicons'; 
+import { SearchBar, Icon } from 'react-native-elements';
+
+
+  const SearchField = () => {
+    const [focused, setFocused] = useState(false);
+    const [search, setSearch] = useState('');
+  
+    const SearchFunction = () => {
+        Alert.alert('Search Result', `Search keyword: ${search}`);
+    };
+  
+    return (
+      <View style={styles.container}>
+        <SearchBar
+          placeholder="Search..."
+          onChangeText={(text) => setSearch(text)}
+          inputContainerStyle={styles.input}
+          value={search}
+          placeholderTextColor="gray"
+          containerStyle={[styles.search]}
+          inputStyle={styles.inputText}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+        />
+        {focused ? (
+          <Icon
+            name="search"
+            type="Feather"
+            size={28}
+            containerStyle={styles.iconContainer}
+            color="black"
+            onPress={SearchFunction} 
+          />
+        ) : null}
+  
+        {!focused ? (
+          <IoniconsIcon
+            name="filter-circle-outline"
+            size={30}
+            color="black"
+            style={styles.Filltericon}
+            onPress={() => {
+              console.log('Filter icon pressed');
+            }}
+          />
+        ) : null}
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      paddingHorizontal: 10,
+      bottom: 4,
+    },
+    inputText: {
+      color: 'black',
+    },
+  
+    iconContainer: {
+      right: 10,
+      top: -47,
+      marginLeft: 352,
+      width: 30,
+      height: 30,
+    },
+  
+    search: {
+      backgroundColor: 'transparent',
+      borderBottomColor: 'transparent',
+      borderTopColor: 'transparent',
+    },
+  
+    Filltericon: {
+      marginLeft: 342,
+      top: -49,
+    },
+  
+    input: {
+      width: 330,
+      height: 45,
+      borderRadius: 23,
+      borderWidth: 2,
+      backgroundColor: 'white',
+      borderBottomWidth: 2,
+      borderColor: 'gray',
+    },
+  });
+  
+  export default SearchField;
