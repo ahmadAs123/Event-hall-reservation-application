ef6f655d5229f094e71d9118c3b6882177525a3c
ahmadAs123 <83554259+ahmadAs123@users.noreply.github.com>
2024-03-22 16:27:46 +0200

    the pages that showed the halls by(map or list)

---
+++ src/ListHalls.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ListHalls = () => {
+  return (
+    <View>
+      <Text>ListHalls</Text>
+    </View>
+  )
+}
+
+export default ListHalls
