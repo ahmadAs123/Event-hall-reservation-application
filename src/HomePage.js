2ecbae82a743a66d4e838857adb4661221d2e59a
ahmadAs123 <83554259+ahmadAs123@users.noreply.github.com>
2024-03-22 16:22:09 +0200

    making homepage of the client with search bar and button navigation bar

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,42 +1,140 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity,ActivityIndicator } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { auth } from '../config';
+import SearchFeild from '../component/SearchFeild';
+import React, { useState, useEffect , useLayoutEffect} from "react";
+import { Icon } from 'react-native-elements'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ClientProfile from './ClientPages/ClientProfile';
+import ClientReservations from './ClientPages/ClientReservations';
+import ClientChat from './ClientPages/ClientChat';
+import ClientSetting from './ClientPages/ClientSetting';
+import { Entypo } from '@expo/vector-icons'; 
 
-const HomePage = () => {
+
+
+const HomePageComponent = () => {
   const navigation = useNavigation();
+  const [loading, setLoading] = useState(false); 
 
-  const handleLogout = async () => {
+  const Logout = async (navigation, setLoading) => {     // function to logout 
     try {
+      setLoading(true)
       await auth.signOut();
       navigation.navigate('StartPage');
     } catch (error) {
-      console.log('Error logging out: ', error);
+      alert('Error  in logging out');
+    }
+    finally {
+      setLoading(false); // return false state 
     }
   };
 
+  
+
   return (
     <View>
-      <TouchableOpacity onPress={handleLogout} style={styles.logoutButton}>
-        <Text style={styles.logoutButtonText}>Logout</Text>
-      </TouchableOpacity>
+      <View style ={styles.TopContainer} > 
+      <SearchFeild />  
+      </View>
+      
+      <TouchableOpacity onPress={Logout} style={styles.logoutButton}>
+      {loading ? (
+          <ActivityIndicator color="white" /> // Show spinner if loggingOut is true
+        ) : (
+          <Text style={styles.logoutButtonText}>Logout</Text>
+        )}
+      </TouchableOpacity> 
     </View>
+  )
+}
+
+
+
+
+const HomePage = () => {
+  const Tab = createBottomTabNavigator();
+  const navigation = useNavigation();
+
+ 
+
+  return (
+    <Tab.Navigator>
+
+<Tab.Screen
+          name="HomePage"
+          component={HomePageComponent}
+          options={{
+            tabBarIcon: ({ color}) => (
+              <Icon name="home" type="AntDesign" color={color} size={30} />
+            ),
+            headerShown: false,
+          }}
+        />
+
+
+<Tab.Screen
+      name="Profile"
+      component={ClientProfile}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Icon name="account-circle" type="MaterialIcons" color={color} size={30} />
+        ),
+        headerShown: false,
+      }}
+    />
+
+
+
+        <Tab.Screen
+      name="Chat"
+      component={ClientChat}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Icon name="chat" color={color} size={30} />
+        ),
+        headerShown: false,
+      }}
+    />
+
+
+        <Tab.Screen
+      name="MyReservations"
+      component={ClientReservations}
+      options={{
+        tabBarIcon: ({ color }) => (
+      <Entypo name="briefcase" color={color} size={30} />        ),
+        headerShown: false,
+      }}
+    />
+        
+    </Tab.Navigator>
+
   )
 }
 
 const styles = {
   logoutButton: {
-    marginTop: 20,
+    marginTop: 250,
     paddingVertical: 10,
     paddingHorizontal: 20,
     backgroundColor: '#00e4d0',
     borderRadius: 10,
     alignItems: 'center',
   },
+  NavBar: {
+   marginTop:259    
+  },
+  TopContainer: {
+    width: 400,
+    marginTop:5,
+    height: 60,
+  }, 
+  
   logoutButtonText: {
     fontWeight: 'bold',
-    fontSize: 24,
-    color: '#fff',
+    color: 'white',
+    fontSize: 23,
   },
 };
 
