d5110d780be128f8059593ddc351b9b8db6f89cf
ahmadAs123 <83554259+ahmadAs123@users.noreply.github.com>
2024-03-22 16:27:05 +0200

    client pages

---
+++ src/ClientPages/ClientChat.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ClientChat = () => {
+  return (
+    <View>
+      <Text>ClientChat</Text>
+    </View>
+  )
+}
+
+export default ClientChat
---
+++ src/ClientPages/ClientReservations.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ClientReservations = () => {
+  return (
+    <View>
+      <Text>ClientReservations</Text>
+    </View>
+  )
+}
+
+export default ClientReservations
