38d386bfeb91075c30926437e365ee3b5924ec83
ahmadAs123 <83554259+ahmadAs123@users.noreply.github.com>
2024-04-01 22:39:51 +0300

    creating init map

--- src/MapHalls.js
+++ src/MapHalls.js
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 const MapHalls = () => {
   return (
-    <View>
-      <Text>MapHalls</Text>
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 31.7683, // Latitude of Jerusalem
+          longitude: 35.2137, // Longitude of Jerusalem
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 31.7683, longitude: 35.2137 }} // Jerusalem coordinates
+          title="Jerusalem"
+          description="Holy City"
+        />
+      </MapView>
     </View>
-  )
-}
+  );
+};
 
-export default MapHalls
+export default MapHalls;
