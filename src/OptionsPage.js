caee1f0849074624c4a3d3b5cdf7c8593c195fdf
ahmadAs123 <83554259+ahmadAs123@users.noreply.github.com>
2024-04-01 22:40:47 +0300

    replace the code with the component

--- src/OptionsPage.js
+++ src/OptionsPage.js
@@ -1,38 +1,17 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MapHalls from './MapHalls';
-import ListHalls from './ListHalls';
-import { Entypo } from '@expo/vector-icons'; 
-
+import { useRoute } from '@react-navigation/native';
+import ButtomNavBar from '../component/ButtomNavBar';
 
 const OptionsPage = () => {
     const Tab = createBottomTabNavigator();
+    const route = useRoute();
+    const  searchValue = route.params.search ; // Get the search value from route params
+
+   
+
   return (
-    <Tab.Navigator>
-<Tab.Screen
-              name="List"
-              component={MapHalls}
-              options={{
-                tabBarIcon: ({color}) => (
-                  <Entypo name="list" color={color} size={30} />
-                ),
-                headerShown: false,
-              }}
-            />
-
-
-<Tab.Screen
-              name="Map"
-              component={ListHalls}
-              options={{
-                tabBarIcon: ({ color}) => (
-                  <Entypo name="map"  color={color} size={30} />
-                ),
-                headerShown: false,
-              }}
-            />
-     </Tab.Navigator>
+    <ButtomNavBar searchValue={searchValue} />
   )
 }
 
