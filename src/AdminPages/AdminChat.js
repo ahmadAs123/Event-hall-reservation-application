fbd23e5dfaada7fcdbbbd6ee1d020ef19b718527
ahmadAs123 <83554259+ahmadAs123@users.noreply.github.com>
2024-08-11 20:32:56 +0300

    add delete chat button + add search bar and  style to the chat card

--- src/AdminPages/AdminChat.js
+++ src/AdminPages/AdminChat.js
@@ -1,92 +1,149 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, FlatList, StyleSheet, Image } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet, Image, TextInput, RefreshControl, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { collection, getDocs, query, orderBy, where } from 'firebase/firestore';
+import { collection, getDocs, query, orderBy, where, doc, deleteDoc } from 'firebase/firestore';
 import { db } from '../../config';
 import { getAuth } from 'firebase/auth';
-
+import Icon from 'react-native-vector-icons/MaterialIcons'; // Importing Icon library
 
 const AdminChat = () => {
   const [halls, setHalls] = useState([]);
+  const [filteredHalls, setFilteredHalls] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [refreshing, setRefreshing] = useState(false);
   const navigation = useNavigation();
   const auth = getAuth();
   const currentUser = auth.currentUser;
+// Navigating to ChatDetails with params
+  const handleHallPress = (hall) => {
+    navigation.navigate('ChatDetails', { user: hall, role: 'admin', image: hall.imageURL });
+  };
 
-    // Navigating to ChatDetails with params
-    const handleHallPress = (hall) => {
-      navigation.navigate('ChatDetails', { user: hall, role: 'admin' , image: hall.imageURL})};
-
-
-  useEffect(() => {
-    const fetchHallsWithLastMessage = async () => {
-      try {
-        const usersArray = [];
-        const hallsRef = collection(db, 'HallsMessages');
-        const hallsSnapshot = await getDocs(hallsRef);
+  const fetchHallsWithLastMessage = async () => {
+    try {
+      const usersArray = [];
+      const hallsRef = collection(db, 'HallsMessages');
+      const hallsSnapshot = await getDocs(hallsRef);
 
         // Loop  on docs 
         for (const doc of hallsSnapshot.docs) {
-          const messagesRef = collection(db, `HallsMessages/${doc.id}/messages`);
-          const q = query(messagesRef, orderBy('createdAt', 'desc'));
-          const messagesSnapshot = await getDocs(q);
+        const messagesRef = collection(db, `HallsMessages/${doc.id}/messages`);
+        const q = query(messagesRef, orderBy('createdAt', 'desc'));
+        const messagesSnapshot = await getDocs(q);
 
-          let imageURL = ''; 
-          let lastNuser = '';  // the  user that chat with  admin
-      
-          if (!messagesSnapshot.empty) {
-            for (const messageDoc of messagesSnapshot.docs) {  // Find the  user who is not the current user and chat with admin
-              const messData = messageDoc.data();
-              if (messData.user?._id !== currentUser.uid) {
-                lastNuser = messData.user?.name;
-                const userQuery = query(collection(db, 'users'), where('userId', '==', messData.user?._id)); // Fetching image URL 
-                const userQuerySnapshot = await getDocs(userQuery);
-                if (!userQuerySnapshot.empty) {
-                  const userData = userQuerySnapshot.docs[0].data();
-                  imageURL = userData.imageURL || ''; 
-                }
-                break; 
-              }}}
+        let imageURL = '';
+        let lastNuser = ''; // the  user that chat with  admin
 
-          usersArray.push({
-            _id: doc.id,
-            name: doc.id,
-            imageURL: imageURL, 
-            lastUser: lastNuser,
-          });
-        }
-        setHalls(usersArray);
-      } catch (error) {
-        console.log('Error while fetching users chating:', error);
+        if (!messagesSnapshot.empty) {
+          for (const messageDoc of messagesSnapshot.docs) {  // Find the  user who is not the current user and chat with admin
+            const messData = messageDoc.data();
+            if (messData.user?._id !== currentUser.uid) {
+              lastNuser = messData.user?.name;
+              const userQuery = query(collection(db, 'users'), where('userId', '==', messData.user?._id)); // Fetching image URL 
+              const userQuerySnapshot = await getDocs(userQuery);
+              if (!userQuerySnapshot.empty) {
+                const userData = userQuerySnapshot.docs[0].data();
+                imageURL = userData.imageURL || '';
+              }
+              break;
+            }}}
+
+        usersArray.push({
+          _id: doc.id,
+          name: doc.id,
+          imageURL: imageURL,
+          lastUser: lastNuser,
+        });
       }
-    };
+      setHalls(usersArray);
+      setFilteredHalls(usersArray);
+    } catch (error) {
+      console.log('Error while fetching users chatting:', error);
+    }
+  };
 
+  useEffect(() => {
     fetchHallsWithLastMessage();
-  }, [currentUser]); 
+  }, [currentUser]);
 
+  useEffect(() => {
+    const filtered = halls.filter(hall =>
+      hall.lastUser.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredHalls(filtered);
+  }, [searchTerm, halls]);
 
+  const onRefresh = () => {
+    setRefreshing(true);
+    fetchHallsWithLastMessage().finally(() => setRefreshing(false));
+  };
 
+  const Deletechat = async (hallId) => {
+    try {
+      await deleteDoc(doc(db, 'HallsMessages', hallId));
+      setHalls(halls.filter(hall => hall._id !== hallId));
+      setFilteredHalls(filteredHalls.filter(hall => hall._id !== hallId));
+    } catch (error) {
+      console.error('Error while  deleting chat:', error);
+    }
+  };
 
+  const DeleteMess = (hallId) => {
+    Alert.alert(
+      'Delete Chat',
+      'Are you sure you want to delete this chat?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Delete',
+          onPress: () => Deletechat(hallId),
+          style: 'destructive',
+        },
+      ],
+      { cancelable: true }
+    );
+  };
 
-  return ( //rendering the list of users using flat list 
-    <View style={styles.container}> 
-        <FlatList
-          data={halls}
-          keyExtractor={item => item._id}
-          renderItem={({ item }) => (
-            <TouchableOpacity style={styles.hallItm} onPress={() => handleHallPress(item)}>
-              <View style={styles.hallItmCont}>
-                <Image
-                  source={{ uri: item.imageURL } }
-                  style={styles.userImg}
-                />
-                <View>
-                  <Text style={styles.UName}>{item.lastUser}</Text>
-                  <Text style={styles.hallName}>{item.name + " Hall"}</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          )}
+  return ( //rendering the list of users using flat list
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Search..."
+          value={searchTerm}
+          onChangeText={setSearchTerm}
         />
+      </View>
+      <FlatList
+        data={filteredHalls}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.card} onPress={() => handleHallPress(item)}>
+            <View style={styles.cardContent}>
+              <Image
+                source={{ uri: item.imageURL }}
+                style={styles.userImage}
+              />
+              <View style={{ flex: 1 }}>
+                <Text style={styles.userName}>{item.lastUser}</Text>
+                <Text style={styles.hallName}>{item.name}</Text>
+              </View>
+              <TouchableOpacity onPress={() => DeleteMess(item._id)}>
+                <Icon name="more-vert" size={24} color="black" />
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        )}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
+      />
     </View>
   );
 };
@@ -94,40 +151,55 @@ const AdminChat = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    padding: 17,
+    padding: 10,
+    backgroundColor: '#f0f0f0',
   },
-
-  hallItmCont: {
-    alignItems: 'center',
+  header: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    marginLeft: 10,
+  },
+  searchContainer: {
+    backgroundColor: '#00e4d4',
+    paddingHorizontal: 19,
+    width: '105%',
+    top: -10,
+    paddingVertical: 11,
+    marginBottom: 6,
+    right: 9,
+  },
+  searchBar: {
+    height: 43,
+    backgroundColor: 'white',
+    borderRadius: 30,
+    paddingLeft: 17,
+    top:1
+  },
+  cardContent: {
     flexDirection: 'row',
-  },
-
-  hallItm: {
-    padding: 16,
-    flexDirection: 'row',
     alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    padding: 10,
   },
- 
-  hallName: {
-    color: '#888',
-    fontSize: 17,
-  },
-  userImg: {
-    width: 61,
+  userImage: {
+    width: 60,
+    height: 60,
     borderRadius: 50,
     marginRight: 10,
-    height: 61,
   },
-
-
-  UName: {
+  card: {
+    backgroundColor: 'white',
+    marginBottom: 9,
+    borderRadius: 11,
+  },
+  userName: {
     fontSize: 18,
     fontWeight: 'bold',
   },
-
+  hallName: {
+    fontSize: 16,
+    color: '#888',
+  },
 });
 
 export default AdminChat;
